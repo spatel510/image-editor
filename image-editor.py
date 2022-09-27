0ce3e8937d0a7a0f9775d4cefcee8b2a0afe5c1f
Shraddha Patel <shr.patel1996@gmail.com>
2022-09-27 16:27:59 -0500

    sharpen images

--- image-editor.py
+++ image-editor.py
@@ -9,7 +9,7 @@ for filename in os.listdir(path):
     img = Image.open(f"{path}/{filename}")
 
     edit = img.filter(ImageFilter.SHARPEN)
-    
+
     clean_name = os.path.splitext(filename)[0]
 
     edit.save(f'.{pathOut}/{clean_name}_edited.jpg')
