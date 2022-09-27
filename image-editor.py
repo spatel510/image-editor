aa1a0eb514a57229a4b0ffd85209c3173227b361
Shraddha Patel <shr.patel1996@gmail.com>
2022-09-27 16:07:23 -0500

    new directory and update python file

--- image-editor.py
+++ image-editor.py
@@ -3,8 +3,13 @@ from PIL import Image, ImageEnhance, ImageFilter
 import os
 
 path = "./images"
-pathOut = "/editedImgs"
+pathOut = "/edited-images"
 
 for filename in os.listdir(path):
     img = Image.open(f"{path}/{filename}")
+
+    edit = img.filter(ImageFilter.SHARPEN)
     
+    clean_name = os.path.splitext(filename)[0]
+
+    edit.save(f'.{pathOut}/{clean_name}_edited.jpg')
